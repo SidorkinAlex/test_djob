820ca132d3fd2ace883a4145f5703e70a3e1202a
SidorkinAlex <palach.orl@mail.ru>
2020-09-15 23:47:31 +0300

    добавлен метод получения списка контактов

---
+++ app/models/Contacts.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MyApp\Models;
+
+use Phalcon\Mvc\Model;
+use Phalcon\Messages\Message;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\InclusionIn;
+
+class Contacts extends Model
+{
+    public function validation()
+    {
+
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,65 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+
+$loader = new Loader();
+$loader->registerNamespaces(
+    [
+        'MyApp\Models' => __DIR__ . '/../app/models/',
+    ]
+);
+file_put_contents('test.log',var_export($loader,1));
+$loader->register();
+
+$container = new FactoryDefault();
+$container->set(
+    'db',
+    function () {
+        return new PdoMysql(
+            [
+                'host'     => 'localhost',
+                'username' => 'docker',
+                'password' => '1111',
+                'dbname'   => 'test',
+            ]
+        );
+    }
+);
+
+
+$app = new Micro($container);
+
+$app->get(
+    '/api/contacts',
+    function () use ($app) {
+        $phql = 'SELECT id, lastName, firstName, middleName '
+            . 'FROM MyApp\Models\Contacts '
+            . 'ORDER BY lastName'
+        ;
+
+        $robots = $app
+            ->modelsManager
+            ->executeQuery($phql)
+        ;
+
+        $data = [];
+
+        foreach ($robots as $robot) {
+            $data[] = [
+                'id'   => $robot->id,
+                'lastName' => $robot->lastName,
+                'firstName' => $robot->firstName,
+                'middleName' => $robot->middleName,
+            ];
+        }
+
+        echo json_encode($data);
+    }
+);
+
+$app->handle(
+    $_SERVER["REQUEST_URI"]
+);
